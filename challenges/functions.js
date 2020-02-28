5626a872d8c71fee234799afb1b6856412999923
janecyyu <janecyyu@gmail.com>
2020-02-28 09:31:30 -0800

    prototypes finished

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,27 +7,36 @@
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
 function consume(a, b, cb){
-  return cb(a, b)
+  return cb(a, b);
 }
-
-console.log(consume(1, 4, (2, 5)));
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+function add() {
+  return this.a + this.b;
+}
 
+function multiply() {
+  return this.a * this.b;
+}
 
+function greeting() {
+  return `Hello ${this.a} ${this.b}, nice to meet you!`
+}
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 
 // Explain in your own words why nestedfunction can access the variable internal.
+//a nested function can "inherit" the arguments and variables of its containing function.
+//In other words, the inner function contains the scope of the outer function.
 
 // Explanation: 
 
